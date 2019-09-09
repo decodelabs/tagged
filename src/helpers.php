60a731e3f1a859c1a9439e2a625d29a499ff0a48
Tom Wright <tom@inflatablecookie.com>
2019-09-09 22:03:10 +0100

    Added boilerplate
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the Tagged package
+ * @license http://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+
+/**
+ * global helpers
+ */
+namespace
+{
+    function html()
+    {
+    }
+}
