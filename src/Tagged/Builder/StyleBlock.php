2355a2ecc4d756eb2a87b171aecd0874bb1dab6c
Tom Wright <tom@inflatablecookie.com>
2019-10-02 23:41:32 +0100

    Added StyleBlock class
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Tagged/Builder/StyleBlock.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * This file is part of the Tagged package
+ * @license http://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+namespace DecodeLabs\Tagged\Builder;
+
+use DecodeLabs\Collections\HashMap;
+use DecodeLabs\Collections\ArrayProvider;
+use DecodeLabs\Collections\Native\HashMapTrait;
+
+use DecodeLabs\Glitch\Inspectable;
+use DecodeLabs\Glitch\Dumper\Entity;
+use DecodeLabs\Glitch\Dumper\Inspector;
+
+class StyleBlock implements \IteratorAggregate, HashMap, Inspectable
+{
+    use HashMapTrait {
+        HashMapTrait::set as private parentSet;
+    }
+
+    const MUTABLE = true;
+
+    /**
+     * Init with styles
+     */
+    public function __construct(...$input)
+    {
+        $this->import(...$input);
+    }
+
+    /**
+     * Import style data
+     */
+    public function import(...$input): StyleBlock
+    {
+        foreach ($input as $data) {
+            if (is_string($data)) {
+                $data = $this->parse($data);
+            } elseif ($data instanceof ArrayProvider) {
+                $data = $data->toArray();
+            } elseif (is_iterable($data) && !is_array($data)) {
+                $data = iterator_to_array($data);
+            } elseif ($data === null) {
+                continue;
+            } elseif (!is_array($data)) {
+                throw Glitch::EInvalidArgument('Invalid style data', null, $data);
+            }
+
+            $this->merge($data);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Parse string styles
+     */
+    protected function parse(string $style): array
+    {
+        $parts = explode('{', $style);
+        $output = [];
+
+        while (!empty($parts)) {
+            $selector = trim(array_shift($parts));
+            $body = explode('}', array_shift($parts), 2);
+            $nextSelector = trim(array_pop($body));
+            $body = trim(array_shift($body));
+
+            if (!empty($nextSelector)) {
+                array_unshift($parts, $nextSelector);
+            }
+
+            $output[$selector] = new StyleList($body);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Direct set a value
+     */
+    public function set(string $key, $value): HashMap
+    {
+        if (!$value instanceof StyleList) {
+            $value = new StyleList($value);
+        }
+
+        return $this->parentSet($key, $value);
+    }
+
+    /**
+     * Render to string
+     */
+    public function __toString(): string
+    {
+        if (empty($this->items)) {
+            return '';
+        }
+
+        $output = [];
+
+        foreach ($this->items as $selector => $styles) {
+            $output[] = $selector.' { '.$styles.' }';
+        }
+
+        return '<style type="text/css">'."\n    ".implode("\n".'    ', $output)."\n".'</style>';
+    }
+
+    /**
+     * Inspect for Glitch
+     */
+    public function glitchInspect(Entity $entity, Inspector $inspector): void
+    {
+        $entity
+            ->setText($this->__toString())
+            ->setSectionVisible('text', false)
+            ->setValues($inspector->inspectList($this->items));
+    }
+}
