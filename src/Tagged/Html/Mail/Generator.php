42b4c9cac9efc2a0088e921601a20d32a7a215d8
Tom Wright <tom@inflatablecookie.com>
2019-11-08 21:21:36 +0000

    Made headings bold
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Tagged/Html/Mail/Generator.php
+++ src/Tagged/Html/Mail/Generator.php
@@ -360,7 +360,7 @@ class Generator
         ],
         'heading' => [
             'margin' => '0 0 15px',
-            'font-weight' => '500',
+            'font-weight' => 'bold',
             'line-height' => '1.2'
         ],
         'p' => [
